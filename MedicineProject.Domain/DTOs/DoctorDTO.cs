cdaaaaa13716a3f4c1ed703978368de46ea6c3b6
mihai <mihai@HOMEPC>
2023-08-25 14:05:16 +0300

    исправление DTO доктора

--- MedicineProject.Domain/DTOs/DoctorDTO.cs
+++ MedicineProject.Domain/DTOs/DoctorDTO.cs
@@ -5,16 +5,34 @@ namespace MedicineProject.Domain.DTOs
 {
     public record DoctorDTO : BaseDTO
     {
+        /// <summary>
+        /// Фамилия доктора
+        /// </summary>
         public string Surname { get; set; }
 
-        public string Patronymic { get; set; }
-
+        /// <summary>
+        /// Отчество доктора не обязательный параметр
+        /// </summary>
+        public string? Patronymic { get; set; }
+        
+        /// <summary>
+        /// Id больницы к которой прикреплён доктор
+        /// </summary>
         public long HospitalId { get; set; }
-
+        
+        /// <summary>
+        /// Id специальности доктора
+        /// </summary>
         public long SpecialityId { get; set; }
 
+        /// <summary>
+        /// Список заявок у доктора
+        /// </summary>
         public List<AppointmentDTO> Appointments { get; set; }
 
+        /// <summary>
+        /// Специальность доктора
+        /// </summary>
         public SpecialityDTO Speciality { get; set; }
 
         public DoctorDTO()
